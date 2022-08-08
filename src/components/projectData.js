a79c46264d610070a83afd73a7a3df3259af6c17
Divine <deevynobeten@gmail.com>
2022-08-08 13:50:03 +0100

    updating resume

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     coverImg: "portfolio-img",
-    projectTitle: "Divine Obeten",
+    projectTitle: "Divine Obeten V1.0",
     techUsed: "HTML, CSS, JAVASCRIPT",
     liveLink: "https://divineobeten.netlify.app/",
     repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
