1a7d44056234133b33df04c59c93e5109bd2230a
Divine <deevynobeten@gmail.com>
2022-08-08 18:03:35 +0100

    updated projects data

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -7,37 +7,4 @@ export default [
     liveLink: "https://divineobeten.netlify.app/",
     repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
   },
-  {
-    id: 1,
-    coverImg: "portfolio-img",
-    projectTitle: "Divine Obeten V1.0",
-    techUsed: "HTML, CSS, JAVASCRIPT",
-    liveLink: "https://divineobeten.netlify.app/",
-    repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
-  },
-  {
-    id: 1,
-    coverImg: "portfolio-img",
-    projectTitle: "Divine Obeten V1.0",
-    techUsed: "HTML, CSS, JAVASCRIPT",
-    liveLink: "https://divineobeten.netlify.app/",
-    repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
-  },
-  {
-    id: 1,
-    coverImg: "portfolio-img",
-    projectTitle: "Divine Obeten V1.0",
-    techUsed: "HTML, CSS, JAVASCRIPT",
-    liveLink: "https://divineobeten.netlify.app/",
-    repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
-  },
-  {
-    id: 1,
-    coverImg: "portfolio-img",
-    projectTitle: "Divine Obeten V1.0",
-    techUsed: "HTML, CSS, JAVASCRIPT",
-    liveLink: "https://divineobeten.netlify.app/",
-    repoLink: "https://github.com/Deevyn9/PORTFOLIO.git",
-  },
 ];
-
