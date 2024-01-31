c16e4ae80b78f1a38bb99bf0c7ea5c2aa33cc027
Divine Obeten <divineobeten9@gmail.com>
2024-01-31 16:38:56 +0100

    Update projectData.js

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,6 +1,14 @@
 const projectData = [
   {
     id: 1,
+    src: "https://images.unsplash.com/photo-1655813710718-00043b177128?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGRvbGxhciUyMHN5bWJvbHxlbnwwfHwwfHx8MA%3D%3D",
+    projectTitle: "Trakkash",
+    techUsed: "NEXTJS, TAILWINDCSS, FIRESTORE, CLERK",
+    liveLink: "https://trakkash.vercel.app",
+    repoLink: "https://github.com/Deevyn9/TraKKash",
+  },
+  {
+    id: 2,
     src: "https://images.unsplash.com/photo-1507627593750-cab2a3684cac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGNoYWlyJTIwc2hvcHxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
     projectTitle: "Comfy Drops",
     techUsed: "REACT, CSS",
@@ -8,7 +16,7 @@ const projectData = [
     repoLink: "https://github.com/Deevyn9/comfy",
   },
   {
-    id: 2,
+    id: 3,
     src: "https://images.unsplash.com/photo-1639653279211-09958a51fb00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZWFydGh8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     projectTitle: "In the World",
     techUsed: "REACT, CSS",
@@ -16,7 +24,7 @@ const projectData = [
     repoLink: "https://github.com/Deevyn9/Where-in-the-world",
   },
   {
-    id: 3,
+    id: 4,
     src: "https://media.istockphoto.com/photos/clipboard-with-green-check-marks-isolated-on-white-background-picture-id1341547247?b=1&k=20&m=1341547247&s=170667a&w=0&h=1kt1ius-qHAKo3MvQDaeDyW9wfzHJyUEq5IK1tdTyWg=",
     projectTitle: "Todo App",
     techUsed: "HTML, CSS, JAVASCRIPT",
@@ -24,7 +32,7 @@ const projectData = [
     repoLink: "https://github.com/Deevyn9/Todo-List",
   },
   {
-    id: 4,
+    id: 5,
     src: "https://images.unsplash.com/photo-1596276020587-8044fe049813?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG9zdGVsfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     projectTitle: "Roommates",
     techUsed: "HTML, CSS, JAVASCRIPT",
@@ -32,7 +40,7 @@ const projectData = [
     repoLink: "https://github.com/Deevyn9/Roommates",
   },
   {
-    id: 5,
+    id: 6,
     src: "https://images.unsplash.com/photo-1646153976497-14925728ff47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaCUyMGNvbXBhbnl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     projectTitle: "Intellitech",
     techUsed: "HTML, CSS, JAVASCRIPT",
